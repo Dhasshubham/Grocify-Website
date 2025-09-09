0ede874d734c82fd56a21b521a38f48cdb8aba0f
Shubham Dhas <shubhamdhas123@gmail.com>
2025-09-09 17:25:13 +0530

    aos

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import Grocery from "../../assets/grocery.png"
-import Button from '../Button/Button'
+import React from "react";
+import Grocery from "../../assets/grocery.png";
+import Button from "../Button/Button";
 
 const Hero = () => {
   return (
-   <section>
-    <div className='max-w-[1400px] mx-auto px-10 md:flex-row flex-col items-center md:pt-25 pt-35  min-h-screen flex'>
+    <section>
+      <div className="max-w-[1400px] mx-auto px-10 md:flex-row flex-col items-center md:pt-25 pt-35  min-h-screen flex">
+        {/* Hero content */}
+        <div className="flex-1">
+          <span className="bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full">
+            Export Best Quality...
+          </span>
 
-      {/* Hero content */}
-      <div className='flex-1'>
-       <span className="bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full">
-  Export Best Quality...
-</span>
+          <h1
+            className="md:text-7xl/20 text-5xl/14 mt-4 font-bold "
+            data-aos="fade-right"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine"
+            data-aos-duration="1500"
+          >
+            Tasty Orange <br />
+            <span className="text-orange-500">Fruits</span> &{" "}
+            <span className="text-orange-500">Veggies</span> <br />
+            In Your City
+          </h1>
+          <p
+            className="text-zinc-600 md:text-lg text-md max-w[530px] mt-5 mb-10"
+            data-aos="fade-zoom-in"
+            data-aos-easing="ease-in-back"
+            data-aos-delay="300"
+            data-aos-offset="0"
+            data-aos-duration="2000"
+          >
+            Bred for a high content of beneficial substances. Our products are
+            all fresh and healthy.
+          </p>
+          <Button content="Shop Now" />
+        </div>
 
-        <h1 className='md:text-7xl/20 text-5xl/14 mt-4 font-bold '>
-            Tasty Orange <br /><span className='text-orange-500' >Fruits</span> & <span className='text-orange-500'>Veggies</span> <br />In Your City
-        </h1>
-        <p className='text-zinc-600 md:text-lg text-md max-w[530px] mt-5 mb-10'>
-           Bred for a high content of beneficial substances. Our products are all fresh and healthy.
-        </p>
-        <Button content="Shop Now"/>
-
+        {/* hero image */}
+        <div className="flex-1">
+          <img src={Grocery} alt="Hero Image" />
+        </div>
       </div>
+    </section>
+  );
+};
 
-
-      {/* hero image */}
-      <div className='flex-1'>
-        <img src={ Grocery} alt="Hero Image" />
-      </div>
-
-    </div>
-   </section>
-  )
-}
-
-export default Hero
+export default Hero;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,86 +1,169 @@
-import React, { useEffect, useState } from 'react'
-import { GoHeartFill } from 'react-icons/go';
+import React, { useEffect, useState } from "react";
+import { GoHeartFill } from "react-icons/go";
 import { HiShoppingBag } from "react-icons/hi2";
 import { IoIosSearch } from "react-icons/io";
 import { TbMenu2 } from "react-icons/tb";
 import { TbMenu3 } from "react-icons/tb";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    const [showMenu , setShowMenu]= useState(false);
-    const [isScrolled , setIsScrolled] =useState(false)
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
 
-    function toggleMenu() {
-        setShowMenu(!showMenu)
-    }
-
-    useEffect(()=>{
-       const handleScroll = ()=> {
-        setIsScrolled(window.scrollY > 10)
-       }
-       window.addEventListener("scroll" , handleScroll);
-       return ()=> window.removeEventListener('scroll' , handleScroll)
-    },[])
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-   <header className={` bg-white fixed top-0 right-0 left-0 z-50 ${isScrolled ? 'drop-shadow-[0_4px_25px_rgba(0,0,0,0.1)]' : ""}`}>
-    <nav className='items-center flex justify-between max-w-[1400px] mx-auto md:h-[14vh] px-10 h-[12vh]'>
+    <header
+      className={` bg-white fixed top-0 right-0 left-0 z-50 ${
+        isScrolled ? "drop-shadow-[0_4px_25px_rgba(0,0,0,0.1)]" : ""
+      }`}
+    >
+      <nav className="items-center flex justify-between max-w-[1400px] mx-auto md:h-[14vh] px-10 h-[12vh]">
         {/* Logo */}
-        <Link to="/" className='text-3xl font-bold'>
-            Gr<span className='text-orange-500 uppercase'>o</span>cify
+        <Link to="/" className="text-3xl font-bold" data-aos="fade-down-right">
+          Gr<span className="text-orange-500 uppercase">o</span>cify
         </Link>
 
         {/* Desktop Menu */}
-        <ul className='hidden items-center gap-x-15 md:flex'>
-            <li><Link to="/" className='font-semibold tracking-wider text-orange-500 hover:text-orange-500'>Home</Link></li>
-            <li><a href="" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>About Us</a></li>
-            <li><a href="" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>Process</a></li>
-            <li><Link to="/contact" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>Contact Us</Link></li>
+        <ul
+          className="hidden items-center gap-x-15 md:flex"
+          data-aos="fade-down-right"  data-aos-duration="1500"
+        >
+          <li>
+            <Link
+              to="/"
+              className="font-semibold tracking-wider text-orange-500 hover:text-orange-500"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <a
+              href=""
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              About Us
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Process
+            </a>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Contact Us
+            </Link>
+          </li>
         </ul>
 
         {/* Nav Action  */}
 
-        <div className='flex items-center gap-x-10'>
+        <div className="flex items-center gap-x-10">
+          {/* input field */}
+          <div className="hidden md:flex p-1 border-2 border-orange-500 rounded-full ">
+            <input
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Search..."
+              autoComplete="off"
+              className="flex-1 height-[5vh] px-3 focus:outline-none ml-1"
+            />
+            <button className="bg-gradient-to-b from-red-600 to-orange-500 text-white w-10 h-10 flex justify-center items-center rounded-full text-xl">
+              <IoIosSearch />
+            </button>
+          </div>
 
-            {/* input field */}
-            <div className='hidden md:flex p-1 border-2 border-orange-500 rounded-full '>
-                <input type="text" name='text' id='text' placeholder='Search...' autoComplete='off' className='flex-1 height-[5vh] px-3 focus:outline-none ml-1'/>
-                <button className='bg-gradient-to-b from-red-600 to-orange-500 text-white w-10 h-10 flex justify-center items-center rounded-full text-xl'><IoIosSearch /></button>
-            </div>
-
-            <a href="#" className='text-zinc-800 text-xl'>
-                <GoHeartFill />
-            </a>
-              <a href="#" className='text-zinc-800 text-xl'>
-                <HiShoppingBag />
-            </a>
-            {/* Hamburger */}
-            <a href="#" className='text-3xl text-zinc-800 md:hidden  ' onClick={toggleMenu}>
-              {showMenu ? <TbMenu3 /> : <TbMenu2 />  }
-            </a>
-
-
+          <a href="#" className="text-zinc-800 text-xl">
+            <GoHeartFill />
+          </a>
+          <a href="#" className="text-zinc-800 text-xl">
+            <HiShoppingBag />
+          </a>
+          {/* Hamburger */}
+          <a
+            href="#"
+            className="text-3xl text-zinc-800 md:hidden  "
+            onClick={toggleMenu}
+          >
+            {showMenu ? <TbMenu3 /> : <TbMenu2 />}
+          </a>
         </div>
 
-             {/* Mobile Menu  */}
-           <ul  className={`md:hidden flex-col gap-y-12 absolute bg-orange-500/20 backdrop-blur-xl rounded-xl p-8  text-center transition-all duration-500 ${
-            showMenu ? "top-[80px] left-1/2 -translate-x-1/2" : "top-[80px] -left-full"
-          } shadow-xl`}  >
-            <li className='p-3'><a href="#" className='font-semibold tracking-wider  text-orange-500 hover:text-orange-600 '>Home</a></li>
-            <li className='p-3'><a href="#" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>About Us</a></li>
-            <li className='p-3'><a href="#" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>Process</a></li>
-            <li className='p-3'><a href="#" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500' >Contact Us</a></li>
+        {/* Mobile Menu  */}
+        <ul
+          className={`md:hidden flex-col gap-y-12 absolute bg-orange-500/20 backdrop-blur-xl rounded-xl p-8  text-center transition-all duration-500 ${
+            showMenu
+              ? "top-[80px] left-1/2 -translate-x-1/2"
+              : "top-[80px] -left-full"
+          } shadow-xl`}
+        >
+          <li className="p-3">
+            <a
+              href="#"
+              className="font-semibold tracking-wider  text-orange-500 hover:text-orange-600 "
+            >
+              Home
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              About Us
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Process
+            </a>
+          </li>
+          <li className="p-3">
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500"
+            >
+              Contact Us
+            </a>
+          </li>
 
-              <li className='md:hidden flex p-1 border-2 border-orange-500 rounded-full '>
-                <input type="text" name='text' id='text' placeholder='Search...' autoComplete='off' className='flex-1 height-[5vh] px-3 focus:outline-none ml-1'/>
-                <button className='bg-gradient-to-b from-red-600 to-orange-500 text-white w-10 h-10 flex justify-center items-center rounded-full text-xl'><IoIosSearch /></button>
-            </li>
-
-
+          <li className="md:hidden flex p-1 border-2 border-orange-500 rounded-full ">
+            <input
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Search..."
+              autoComplete="off"
+              className="flex-1 height-[5vh] px-3 focus:outline-none ml-1"
+            />
+            <button className="bg-gradient-to-b from-red-600 to-orange-500 text-white w-10 h-10 flex justify-center items-center rounded-full text-xl">
+              <IoIosSearch />
+            </button>
+          </li>
         </ul>
-    </nav>
-   </header>
-  )
-}
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
